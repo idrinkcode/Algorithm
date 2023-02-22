2d4a0c397d63058e6f928f00f65b75c27d20762b
idrinkcode <sceks96@gmail.com>
2023-02-22 10:28:42 +0900

    올라가라

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        class Solution {
+            public int solution(int num1, int num2) {
+                return num1 * num2;
+            }
+        }
+    }
+}
